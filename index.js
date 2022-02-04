259fb7b2066b206325cabc94618066aa9afce5f4
Janne Korkee <janne.korkee@eficode.com>
2022-02-04 14:58:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const password="metsäGr0uppiRuulz"
+console.log(password)
+
+
+function lostInTheWoods(arg) {
+  let turhapuro = "uuno"
+}
+
+puuttuuVarConstLet = "not Explicit is not good practices";
